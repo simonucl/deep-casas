0e738b2ac87935bbc056a3c3c1e4c1c5f831abe6
simonucl <simon011130@gmail.com>
2022-10-27 18:05:20 +0100

    Fixed the Ensemble model issue and add CNN-BiLSTM model

--- models.py
+++ models.py
@@ -1,65 +1,101 @@
 #!/usr/bin/env python3
 
-from keras.layers import Dense, LSTM, Bidirectional, Concatenate
+from ast import Mod
+from pyexpat import model
+from unicodedata import name
+from keras.layers import Dense, LSTM, Bidirectional, concatenate, Input, Conv1D, Dropout, MaxPool1D, Flatten
 from keras.layers.embeddings import Embedding
 from keras.models import Sequential
+from keras import Model
 
 
-def get_LSTM(input_dim, output_dim, max_lenght, no_activities):
+def get_LSTM(input_dim, output_dim, max_lenght, no_activities, dropout=0.0):
     model = Sequential(name='LSTM')
+    # model.add(Input(shape=(input_dim,)))
     model.add(Embedding(input_dim, output_dim, input_length=max_lenght, mask_zero=True))
-    model.add(LSTM(output_dim))
+    model.add(LSTM(output_dim, dropout=dropout))
     model.add(Dense(no_activities, activation='softmax'))
     return model
 
 
-def get_biLSTM(input_dim, output_dim, max_lenght, no_activities):
+def get_biLSTM(input_dim, output_dim, max_lenght, no_activities, dropout=0.0):
     model = Sequential(name='biLSTM')
     model.add(Embedding(input_dim, output_dim, input_length=max_lenght, mask_zero=True))
-    model.add(Bidirectional(LSTM(output_dim)))
+    model.add(Bidirectional(LSTM(output_dim, dropout=dropout)))
     model.add(Dense(no_activities, activation='softmax'))
     return model
 
 
-def get_Ensemble2LSTM(input_dim, output_dim, max_lenght, no_activities):
-    model1 = Sequential()
-    model1.add(Embedding(input_dim, output_dim, input_length=max_lenght, mask_zero=True))
-    model1.add(Bidirectional(LSTM(output_dim)))
+def get_Ensemble2LSTM(input_dim, output_dim, max_lenght, no_activities, dropout=0.0):
+    in_layer1 = Input(shape=(input_dim[2],))
+    model1 = Embedding(input_dim[1], output_dim, input_length=max_lenght, mask_zero=True)(in_layer1)
+    model1 = Bidirectional(LSTM(output_dim, dropout=dropout))(model1)
 
-    model2 = Sequential()
-    model2.add(Embedding(input_dim, output_dim, input_length=max_lenght, mask_zero=True))
-    model2.add(LSTM(output_dim))
+    in_layer2 = Input(shape=(input_dim[2],))
+    model2 = (Embedding(input_dim[1], output_dim, input_length=max_lenght, mask_zero=True))(in_layer2)
+    model2 = LSTM(output_dim, dropout=dropout)(model2)
 
-    model = Sequential(name='Ensemble2LSTM')
-    model.add(Concatenate()([model1, model2]))
-    model.add(Dense(no_activities, activation='softmax'))
-    return model
+    model = concatenate([model1, model2])
 
+    model = (Dense(no_activities, activation='softmax')) (model)
 
-def get_CascadeEnsembleLSTM(input_dim, output_dim, max_lenght, no_activities):
-    model1 = Sequential()
-    model1.add(Embedding(input_dim, output_dim, input_length=max_lenght, mask_zero=True))
-    model1.add(Bidirectional(LSTM(output_dim, return_sequences=True)))
-
-    model2 = Sequential()
-    model2.add(Embedding(input_dim, output_dim, input_length=max_lenght, mask_zero=True))
-    model2.add(LSTM(output_dim, return_sequences=True))
-
-    model = Sequential(name='CascadeEnsembleLSTM')
-    model.add(Concatenate()([model1, model2]))
-    model.add(LSTM(output_dim))
-    model.add(Dense(no_activities, activation='softmax'))
-    return model
+    return Model(inputs=[in_layer1, in_layer2], outputs=model, name='Ensemble2LSTM')
 
 
-def get_CascadeLSTM(input_dim, output_dim, max_lenght, no_activities):
+def get_CascadeEnsembleLSTM(input_dim, output_dim, max_lenght, no_activities, dropout=0.0):
+    n_timesteps, n_features = input_dim[1], input_dim[2]
+    in_layer1 = Input(shape=(n_features, ))
+    model1 = (Embedding(n_timesteps, output_dim, input_length=max_lenght, mask_zero=True))(in_layer1)
+    model1 = (Bidirectional(LSTM(output_dim, return_sequences=True, dropout=dropout)))(model1)
+
+    in_layer2 = Input(shape=(n_features, ))
+    model2 = (Embedding(input_dim, output_dim, input_length=max_lenght, mask_zero=True))(in_layer2)
+    model2 = (LSTM(output_dim, return_sequences=True, dropout=dropout))(model2)
+
+    model = concatenate([model1, model2])
+    model = (LSTM(output_dim, dropout=dropout))(model)
+    model = (Dense(no_activities, activation='softmax'))(model)
+    return Model(inputs=[in_layer1, in_layer2], outputs=model, name='CascadeEnsembleLSTM')
+
+
+def get_CascadeLSTM(input_dim, output_dim, max_lenght, no_activities, dropout=0.0):
     model = Sequential(name='CascadeLSTM')
     model.add(Embedding(input_dim, output_dim, input_length=max_lenght, mask_zero=True))
-    model.add(Bidirectional(LSTM(output_dim, return_sequences=True)))
-    model.add(LSTM(output_dim))
+    model.add(Bidirectional(LSTM(output_dim, return_sequences=True, dropout=dropout)))
+    model.add(LSTM(output_dim, dropout=dropout))
     model.add(Dense(no_activities, activation='softmax'))
     return model
 
+def get_CNN_biLSTM(input_dim, output_dim, max_lenght, no_activities, dropout=0.0, seed=7):
+    n_timesteps, n_features = input_dim[1], input_dim[2]
+    in_layer1 = Input(shape=(n_features, ))
+    model1 = (Conv1D(filters = 64, kernel_size = 3, activation='relu', input_shape=(n_timesteps, n_features)))(in_layer1)
+    model1 = (Conv1D(filters = 32, kernel_size = 3, activation='relu', input_shape=(n_timesteps, n_features)))(model1)
+    model1 = Dropout(.5, seed=seed)(model1)
+    model1 = MaxPool1D(pool_size=2)(model1)
+    model1 = Flatten()(model1)
+
+    
+    in_layer2 = Input(shape=(n_features, ))
+    model2 = (Conv1D(filters = 64, kernel_size = 7, activation='relu', input_shape=(n_timesteps, n_features)))(in_layer2)
+    model2 = (Conv1D(filters = 32, kernel_size = 7, activation='relu', input_shape=(n_timesteps, n_features)))(model2)
+    model2 = Dropout(.5, seed=seed)(model2)
+    model2 = MaxPool1D(pool_size=2)(model2)
+    model2 = Flatten()(model2)
+
+    in_layer3 = Input(shape=(n_features, ))
+    model3 = (Conv1D(filters = 64, kernel_size = 11, activation='relu', input_shape=(n_timesteps, n_features)))(in_layer3)
+    model3 = (Conv1D(filters = 32, kernel_size = 11, activation='relu', input_shape=(n_timesteps, n_features)))(model3)
+    model3 = Dropout(.5, seed=seed)(model3)
+    model3 = MaxPool1D(pool_size=2)(model3)
+    model3 = Flatten()(model3)
+
+    model = concatenate([model1, model2, model3])
+    model = (Bidirectional(LSTM(64, return_sequences=True)))(model)
+    model = (Bidirectional(LSTM(32, return_sequences=True)))(model)
+
+    model = (Dense(no_activities, activation='softmax'))(model)
+    return Model(inputs=[in_layer1, in_layer2, in_layer3], outputs=model, name='CNN-BiLSTM')
 
 def compileModel(model):
     model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
--- train.py
+++ train.py
@@ -5,19 +5,21 @@ import csv
 from datetime import datetime
 
 import numpy as np
-from keras.callbacks import ModelCheckpoint, CSVLogger
+from keras.callbacks import ModelCheckpoint, CSVLogger, EarlyStopping
 from sklearn.metrics import confusion_matrix, classification_report
-from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import StratifiedKFold, TimeSeriesSplit
 from sklearn.utils import compute_class_weight
 
 import data_hh
 import models
 import sys
+import os
 
 # fix random seed for reproducibility
 seed = 7
-units = 64
-epochs = 200
+units = 128
+epochs = 50
+dropout = 0.2
 
 if __name__ == '__main__':
     """The entry point"""
@@ -35,10 +37,10 @@ if __name__ == '__main__':
         cvaccuracy = []
         cvscores = []
         modelname = ''
-
+        tsv = TimeSeriesSplit(n_splits=3)
         kfold = StratifiedKFold(n_splits=3, shuffle=True, random_state=seed)
         k = 0
-        for train, test in kfold.split(X, Y):
+        for train, test in tsv.split(X, Y):
             print('X_train shape:', X[train].shape)
             print('y_train shape:', Y[train].shape)
 
@@ -46,7 +48,7 @@ if __name__ == '__main__':
             args_model = str(args.model)
 
             if 'Ensemble' in args_model:
-                input_dim = len([X[train], X[train]])
+                input_dim = np.array([X[train], X[train]]).shape
                 X_train_input = [X[train], X[train]]
                 X_test_input = [X[test], X[test]]
             else:
@@ -56,33 +58,38 @@ if __name__ == '__main__':
             no_activities = len(dictActivities)
 
             if args_model == 'LSTM':
-                model = models.get_LSTM(input_dim, units, data_hh.max_lenght, no_activities)
+                model = models.get_LSTM(input_dim, units, data_hh.max_lenght, no_activities, dropout=dropout)
             elif args_model == 'biLSTM':
-                model = models.get_biLSTM(input_dim, units, data_hh.max_lenght, no_activities)
+                model = models.get_biLSTM(input_dim, units, data_hh.max_lenght, no_activities, dropout=dropout)
             elif args_model == 'Ensemble2LSTM':
-                model = models.get_Ensemble2LSTM(input_dim, units, data_hh.max_lenght, no_activities)
+                model = models.get_Ensemble2LSTM(input_dim, units, data_hh.max_lenght, no_activities, dropout=dropout)
             elif args_model == 'CascadeEnsembleLSTM':
-                model = models.get_CascadeEnsembleLSTM(input_dim, units, data_hh.max_lenght, no_activities)
+                model = models.get_CascadeEnsembleLSTM(input_dim, units, data_hh.max_lenght, no_activities, dropout=dropout)
             elif args_model == 'CascadeLSTM':
-                model = models.get_CascadeLSTM(input_dim, units, data_hh.max_lenght, no_activities)
+                model = models.get_CascadeLSTM(input_dim, units, data_hh.max_lenght, no_activities, dropout=dropout)
             else:
                 print('Please get the model name '
                       '(eg. --v [LSTM | biLSTM | Ensemble2LSTM | CascadeEnsembleLSTM | CascadeLSTM])')
                 exit(-1)
 
             model = models.compileModel(model)
+            # sys.exit(1)
             modelname = model.name
 
             checkpoint_filepath = './tmp/checkpoint/'
 
             currenttime = datetime.utcnow().strftime('%Y%m%d-%H%M%S')
+            checkpoint_filepath += model.name + '-' + str(currenttime) + '/'
+            os.mkdir(checkpoint_filepath)
+
             csv_logger = CSVLogger(
-                checkpoint_filepath + model.name + '-' + dataset + '-' + str(currenttime) + '.csv')
+                checkpoint_filepath + model.name + '-' + dataset + '-fold' + str(k + 1) + '.csv')
             model_checkpoint = ModelCheckpoint(
-                checkpoint_filepath + model.name + '-' + dataset + '-' + str(currenttime) + '.h5',
+                checkpoint_filepath + model.name + '-' + dataset + '-fold' + str(k + 1) + '.hdf5',
                 monitor='val_accuracy',
                 mode='max',
                 save_best_only=True)
+            early_stopping_callback = EarlyStopping(monitor='val_loss', patience=5, min_delta=0.00001)
 
             # train the model
             print('Begin training ...')
@@ -90,7 +97,7 @@ if __name__ == '__main__':
                                                 y=Y)  # use as optional argument in the fit function
 
             model.fit(X_train_input, Y[train], validation_split=0.2, epochs=epochs, batch_size=64, verbose=1,
-                      callbacks=[csv_logger, model_checkpoint])
+                      callbacks=[early_stopping_callback, csv_logger, model_checkpoint])
 
             # evaluate the model
             print('Begin testing ...')
