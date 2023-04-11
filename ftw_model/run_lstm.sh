fb8fcbfc0fe46783aa4ea7c09e281c78e7e4d29c
simon <simonucl>
2023-04-11 21:01:42 +0100

    Add run file

--- ftw_model/run_lstm.sh
+++ ftw_model/run_lstm.sh
@@ -1,5 +1,6 @@
 encoding_methods=('mean_with_weight' '1d_cnn')
-encoding_methods=('1d_cnn')
+
+encoding_methods=('mean_with_weight')
 batch_size=(32)
 
 deltas=(10)
@@ -16,8 +17,8 @@ do
     #     continue
     # fi
     
-    # run only when i == 1, 4, 5, 6, 7
-    # if (($i != 1 && $i != 4 && $i != 5 && $i != 6 && $i != 7)); then
+    # run only when i == 2
+    # if (($i != 1)); then
     #     continue
     # fi
 
@@ -28,7 +29,7 @@ do
         python extract_feature.py --input "../hh_dataset/hh1$i1/hh1$i1.ann.txt" --delta ${delta} --window FIB_FTWs --output "../hh_dataset/hh_npy/fib_hh1$i1.npy"
         for feature_encoding in ${encoding_methods[@]}; do
             for batch in ${batch_size[@]}; do
-                python train.py --model BiLSTM --features ../hh_dataset/hh_npy/fib_hh1${i1}_feature.npy --activities ../hh_dataset/hh_npy/fib_hh1${i1}_activity.npy --feature_encoding ${feature_encoding} --delta ${delta} --file_ext '_overlook_merged_with_time2vec_' --batch ${batch}
+                python train.py --model BiLSTM --features ../hh_dataset/hh_npy/fib_hh1${i1}_feature.npy --activities ../hh_dataset/hh_npy/fib_hh1${i1}_activity.npy --feature_encoding ${feature_encoding} --delta ${delta} --file_ext '_merged' --batch ${batch}
             done
             # python train.py --model BiLSTM --features ../hh_dataset/hh_npy/fib_hh1${i1}_feature.npy --activities ../hh_dataset/hh_npy/fib_hh1${i1}_activity.npy --feature_encoding ${feature_encoding} --delta ${delta} --file_ext '_unmerged_with_time2vec'
         done
